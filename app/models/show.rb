8204c8bfa8f17a6b72147e13a21cf458496fbb65
Henna Vahatiitto <hnnamv@gmail.com>
2020-03-04 19:42:41 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,4 +4,28 @@ class Show < ActiveRecord::Base
     
   end
   
+  def self.highest_rating
+    
+  end
+  
+  def self.highest_rating
+    
+  end
+  
+  def self.highest_rating
+    
+  end
+  
+  def self.highest_rating
+    
+  end
+  
+  def self.highest_rating
+    
+  end
+  
+  def self.highest_rating
+    
+  end
+  
 end
