86b63d3283a5172641e02cbe9237dd84efaf47cb
Henna Vahatiitto <hnnamv@gmail.com>
2020-03-04 19:34:47 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,5 +1,9 @@
 class CreateShows < Active_Record::Migrate[5.1]
   
   def change
-    create_table :shows
+    create_table :shows do |t|
+      t.name :string
+      t.network
+      t.day 
+      t.rating
 end
