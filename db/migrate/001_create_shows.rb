5c163483ed3ba5611ccc65b15cb438bea69af691
Henna Vahatiitto <hnnamv@gmail.com>
2020-03-04 19:49:36 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,30 +1,11 @@
-class Show < ActiveRecord::Base
-
-    def self.highest_rating
-      self.maximum(:rating)
-    end
-  
-    def self.most_popular_show
-      self.where("rating = ?", self.highest_rating).first
-    end
-  
-    def self.lowest_rating
-      self.minimum(:rating)
-    end
-  
-    def self.least_popular_show
-      self.where("rating = ?", self.lowest_rating).first
-    end
-  
-    def self.ratings_sum
-      self.sum(:rating)
-    end
-    
-    def self.popular_shows
-      self.where("rating >= ?", 5)
-    end
-  
-    def self.shows_by_alphabetical_order
-      self.order(:name)
+class CreateShows < ActiveRecord::Migration
+  def change
+    create_table :shows do |t|
+      #primary key of :id is created for us!
+      t.string :name
+      t.string :day
+      t.string :network
+      t.integer :rating
     end
   end
+end
