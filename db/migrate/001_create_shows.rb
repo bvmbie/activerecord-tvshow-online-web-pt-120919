a84e5146632c7a931ae607cdec5192436e804af0
Henna Vahatiitto <hnnamv@gmail.com>
2020-03-04 19:49:44 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,7 +1,6 @@
 class CreateShows < ActiveRecord::Migration
   def change
     create_table :shows do |t|
-      #primary key of :id is created for us!
       t.string :name
       t.string :day
       t.string :network
