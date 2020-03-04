054432d9ea8374893bc72b0fd721843b4aae4ef2
Henna Vahatiitto <hnnamv@gmail.com>
2020-03-04 19:49:09 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,31 +1,30 @@
-class Show < ActiveRecord::Base 
+class Show < ActiveRecord::Base
+
+    def self.highest_rating
+      self.maximum(:rating)
+    end
   
-  def self.highest_rating
-    self.maximum(:rating)
-  end
+    def self.most_popular_show
+      self.where("rating = ?", self.highest_rating).first
+    end
   
-  def self.most_popular_show
-    self.highest_rating.first
-  end
+    def self.lowest_rating
+      self.minimum(:rating)
+    end
   
-  def self.lowest_rating
+    def self.least_popular_show
+      self.where("rating = ?", self.lowest_rating).first
+    end
+  
+    def self.ratings_sum
+      self.sum(:rating)
+    end
     
-  end
+    def self.popular_shows
+      self.where("rating >= ?", 5)
+    end
   
-  def self.least_popular_show
-    
+    def self.shows_by_alphabetical_order
+      self.order(:name)
+    end
   end
-  
-  def self.ratings_sum
-    
-  end
-  
-  def self.popular_shows
-    
-  end
-  
-  def self.shows_by_alphabetical_order
-    
-  end
-  
-end
